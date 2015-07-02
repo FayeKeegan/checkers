f9770b271eb32a516f386eca2766240830aee9b9
Faye Keegan <faye.keegan@gmail.com>
2015-07-02 11:12:50 -0700

    get move differentials

--- piece.rb
+++ piece.rb
@@ -1,5 +1,15 @@
 class Piece
 
+	SLIDE_DIFFS =[[-1,1],
+				[-1, -1],
+				[ 1, -1],
+				[ 1,  1]]	
+
+	JUMP_DIFFS =[[-2, 2],
+				[-2, -2],
+				[ 2, -2],
+				[-2,  2]]
+
 	attr_accessor :pos, :board
 	attr_reader :color
 
@@ -12,6 +22,22 @@ class Piece
 
 	def is_king?
 		@is_king
+	end
+
+	def make_king
+		@is_king = true
+	end
+
+	def perform_slide(end_pos)
+	end
+
+	def perform_jump(end_pos)
+	end
+
+
+	def move_diffs
+		return SLIDE_DIFFS + JUMP_DIFFS if is_king?
+		color == :red? ? SLIDE_DIFFS.take(2) + JUMP_DIFFS.take(2) : SLIDE_DIFFS.drop(2) + JUMP_DIFFS.drop()
 	end
 
 
